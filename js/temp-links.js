17566945c4382eb641feeef3e8d009b9998c9ef2
Michael Dornic <michael.dornic@gmail.com>
2015-01-12 18:30:52 +0000

    update landing 2

---
+++ js/temp-links.js
@@ -0,0 +1,3 @@
+$('.error').click(function() {
+	alert("Le site est toujours en construction. En conséquence, la page ou le document auquel vous cherchez à acceder n'est pas encore disponible. Pour plus d'informations, veuillez consulter l'ancien site (lien en haut à gauche de cette page) ou bien me contacter par mail. Bonne visite!" );
+});
