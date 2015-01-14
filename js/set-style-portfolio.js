60cfd01027eb18a1c786dc873509a6102a7b8dad
Michael Dornic <michael.dornic@gmail.com>
2015-01-14 19:18:42 +0000

    portfoliov2

---
+++ js/set-style-portfolio.js
@@ -0,0 +1,20 @@
+page = $(".core.portfolio");
+
+function set_style_portfolio(p) {
+	span = p.children('.block_container').children('.block').children('a').children('span');
+	span.css('background', 'rgba(87,154,197,1)');
+
+	p.children('.block_container').each(function() {
+		$(this).children('.block').each(function() {
+			$(this).children('a').children('span').css('height', $(this).children('a').children('img').css('height'));
+			$(this).children('a').children('span').css('width', $(this).children('a').children('img').css('width'));
+		});
+	});
+	span.css('display', 'none');
+}
+
+window.onresize = function() {
+    set_style_portfolio(page)
+};
+
+set_style_portfolio(page)
